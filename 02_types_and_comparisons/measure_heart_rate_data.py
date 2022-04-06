c9f448b24a2413d42d05f16cba62be80416f231f
EdinZolj <edinzolj@gmx.de>
2022-04-06 16:09:53 +0200

    Measure Heart Rate Data

---
+++ 02_types_and_comparisons/measure_heart_rate_data.py
@@ -0,0 +1,12 @@
+#Program that analyzes a patients heard rate. Using comparisons to check if the heart rate is too low or too high and tell the patient if they schould worry about their health.
+
+heart_rate = 77
+
+too_low = heart_rate < 60
+too_high = heart_rate > 100
+
+print("Heart rate low:")
+print(too_low)
+
+print("Heart rate high:")
+print(too_high)
