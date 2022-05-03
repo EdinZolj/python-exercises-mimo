4a737ba67534f636e6a3763a29bf0733f0a978d9
EdinZolj <edinzolj@gmx.de>
2022-05-03 17:26:52 +0200

    New exercise

---
+++ 05_organizing_data_(lists)/looping_over_lists.py
@@ -0,0 +1,20 @@
+
+final_scores = [17, 22, 34, 13]
+for score in final_scores:
+    print(score)
+
+
+artists = ["Chagall", "Lissitzky"]
+for artist in artists:
+    print(artist)
+    print("-----")
+
+
+student_grades = ["A", "A-", "C"]
+for grade in student_grades:
+    print(grade)
+
+
+items = ["milk", "tomato", "apple"]
+for item in items:
+    print(item)
