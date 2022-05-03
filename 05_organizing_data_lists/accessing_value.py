0108066a7e7398bba1e98d6a1e13025b0668d876
EdinZolj <edinzolj@gmx.de>
2022-05-03 17:45:01 +0200

    New exercise

---
+++ 05_organizing_data_lists/accessing_value.py
@@ -0,0 +1,12 @@
+michael = 13
+anna = 20
+tina = 5
+candidates = [michael, anna, tina]
+print(candidates[tina]) #prüfen ob funktioniert mit Namen und = "5" ???
+
+
+first = "John"
+second = "Joseph"
+third = "Donnie"
+winners = [first, second, third]
+print(winners[2])
