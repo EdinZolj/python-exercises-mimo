cde3cdfd85c755c007667dacb45f4753f608c8f3
EdinZolj <edinzolj@gmx.de>
2022-05-03 17:37:04 +0200

    New exercise

---
+++ 05_organizing_data_(lists)/deciding_with_lists.py
@@ -0,0 +1,11 @@
+#Count the elements in a list using if-statement
+
+users = ["Sarah", "Mike", "Ella"]
+print(len(users))
+
+tasks = ["dishes", "windows", "vaccum"]
+if len(tasks) > 0:
+    print("Ugh, more work!")
+
+subway_lines = ["U1", "U3", "U6"]
+number_of_lines = len(subway_lines)
