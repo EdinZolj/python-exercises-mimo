0b4f3a4f8d08a10c341cd598a7ff72c01236e9ad
EdinZolj <edinzolj@gmx.de>
2022-05-04 15:52:20 +0200

    New exercise

---
+++ 03_conditional_statements/password_enough_characters.py
@@ -0,0 +1,9 @@
+#Program checks if we have enough characters for a password.
+
+password_length = 8
+input_password = 6
+
+if input_password < password_length:
+    print("Not enough characters.")
+else:
+    print("Perfect password length!")
