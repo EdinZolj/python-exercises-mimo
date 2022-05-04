3ea7be1fb371f1080155e355a4961c6000ee173a
EdinZolj <edinzolj@gmx.de>
2022-05-04 15:47:38 +0200

    New exercise

---
+++ 03_conditional_statements/compare_body_temperatures.py
@@ -0,0 +1,12 @@
+#Program that checks if a body is in a healthy range.
+
+low = 97
+high = 99
+temp = 101.2
+
+if temp <= high and temp >= low:
+    print("You're in healthy range!")
+if temp < low:
+    print("Your temparature is low.")
+if temp > high:
+    print("Your temperature is high.")
