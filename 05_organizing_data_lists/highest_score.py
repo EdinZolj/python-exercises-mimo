3b1e2b98b6df74a79b9dbdfe5f7df23f2b00828d
EdinZolj <edinzolj@gmx.de>
2022-05-05 14:44:38 +0200

    New exercise

---
+++ 05_organizing_data_lists/highest_score.py
@@ -0,0 +1,10 @@
+#Code that helps an arcade machine find the highest score from a list of scores.
+
+user_score = [12, 42, 55, 100, 11, 22]
+highest = user_score[0]
+
+for score in user_score:
+    if score > highest:
+        highest = score
+
+print(f"Highest score: {highest}")
---
+++ 05_organizing_data_lists/stocks_tracker.py
@@ -0,0 +1,15 @@
+#A list by using indices to manage andd update a list of company's stock values.
+#By storing the latest, highest, and lowest stock value and update when new values are recorded.
+
+apple_stocks = [298.18, 304.18, 289.23]
+
+apple_stocks[0] = 310
+print("latest value:")
+print(apple_stocks[0])
+
+apple_stocks[1] = 310
+print("highest value:")
+print(apple_stocks[1])
+
+print("lowest value:")
+print(apple_stocks[2])
