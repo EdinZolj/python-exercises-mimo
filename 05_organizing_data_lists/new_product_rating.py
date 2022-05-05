cfc72ad53795f4b295877ccc0e2fdc9934a92d89
EdinZolj <edinzolj@gmx.de>
2022-05-05 14:27:46 +0200

    New exercise

---
+++ 05_organizing_data_lists/new_product_rating.py
@@ -0,0 +1,13 @@
+#A list to store the results of how new yogurt flavors were rated in a customer test and the final results.
+
+flavors = ["cinnamon", "pumpkin", "apple pie"]
+print("New flavors:")
+print(flavors)
+
+ratings = [4, 2.5, 3]
+print("Consumer ratings:")
+print(ratings)
+
+passed = [True, False, True]
+print("Release:")
+print(passed)
