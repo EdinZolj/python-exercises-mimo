c80dab9143bd8fc95989fe1952c552ea84a24b64
EdinZolj <edinzolj@gmx.de>
2022-05-05 17:32:11 +0200

    New exercise

---
+++ 07_using_strings/splitting_strings.py
@@ -0,0 +1,15 @@
+#When working with different kind of data, we'll often receive it in a format that can make it hard to work with.
+
+new_users = "Ann Jon Edin"
+users_list = new_users.split()
+print(users_list)
+
+#we can specify exactly how we want to split a string by placing a separator inside the parentheses, like with "," here:
+
+user = "Lauren,25,F,Architect"
+user_1 = user.split(",")
+print(user_1)
+
+path = "getmimo.com/glossary/python"
+path_list = path.split("/")
+print(path_list)
