afa8edfa41af136fd8f64ad2e1ba740b9604e020
EdinZolj <edinzolj@gmx.de>
2022-05-05 14:00:12 +0200

    New exercise

---
+++ 03_conditional_statements/complete_experiment_check.py
@@ -0,0 +1,10 @@
+#Program checks if an experiment is complete.
+
+trials = 5
+data_points = 160
+peer_reviewed = True
+
+if trials >= 5 and data_points > 100 and peer_reviewed:
+    print("Experiment finished!")
+else:
+    print("There's more work to do.")
