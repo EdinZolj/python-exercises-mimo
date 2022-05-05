1fcda777c3d6f5e8e321ea756853c393d438228d
EdinZolj <edinzolj@gmx.de>
2022-05-05 14:58:45 +0200

    New exercise

---
+++ 05_organizing_data_lists/humidity_monitor.py
@@ -0,0 +1,8 @@
+#Program manage soil humidity data received from a greenhouse monitor. Using the list to track and update at over five days.
+
+humidity_level = [87, 83, 89, 88, 87]
+
+humidity_level.insert(0, 90)    #insert value "90" in index 0
+humidity_level.pop()            #delete last value in list to keep "just 5 values in total in list"
+
+print(humidity_level)
