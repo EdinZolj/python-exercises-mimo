8e8389647cbf27aeb712254648b3dd21af3cc46c
EdinZolj <edinzolj@gmx.de>
2022-05-05 17:44:27 +0200

    New exercise

---
+++ 07_using_strings/updating_strings.py
@@ -0,0 +1,10 @@
+#We can replace a part of a string stored inside a variable by first coding the variables name and the instruction "replace()".
+
+special = "Todays special is pasta"
+new_special = special.replace("pasta", "pizza")
+print(special)
+print(new_special)
+
+special = "Todays special is pasta"
+special = special.replace("pasta", "pizza")
+print(special)
