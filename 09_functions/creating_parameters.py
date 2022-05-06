ce7d8e00d2740083a73ea767fbae34292a1fad4d
EdinZolj <edinzolj@gmx.de>
2022-05-06 14:08:47 +0200

    New exercise

--- 09_functions/creating_parameters.py
+++ 09_functions/creating_parameters.py
@@ -24,3 +24,10 @@ def show_winners(first, second, third):
     print("Third place: " + third)
 
 show_winners("Andrina", "Edin", "Edina")
+
+
+def create_email(name, year):
+    return name + year + "@hutmail.com"
+
+email = create_email("jo", "1998")
+print(email)
