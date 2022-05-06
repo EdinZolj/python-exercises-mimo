b6c36288f033ddbb0513d8ba4395ad94a197c201
EdinZolj <edinzolj@gmx.de>
2022-05-06 12:56:57 +0200

    New exercise

---
+++ 09_functions/reusing_code.py
@@ -0,0 +1,13 @@
+#Instead of rewriting the same code, we can use functions to group related code and perform the task in one place.
+# A colon ":" marks the beginning of the code block that belongs to the function.
+#To run the code, we need to call the function. We do that by coding the name followed by parentheses, like greet_user()
+
+def greet_user():
+    print("Good morning Anna!")
+greet_user()
+
+def night_routine():
+    print("Reflect")
+    print("Light off")
+    print("Alarm set")
+night_routine()
