f23aab24993542674eafb13eef6a394295def1d8
EdinZolj <edinzolj@gmx.de>
2022-05-06 10:08:01 +0200

    New exercise

--- 07_using_strings/f_strings.py
+++ 07_using_strings/f_strings.py
@@ -22,3 +22,25 @@ print(f"Tax: € {tax}")
 
 total = bill + tip + tax
 print(f"Total: € {total}")
+
+#Keep track of two players and the data they generate during a round-based game.
+
+player_1 = "Sam"
+player_2 = "Edin"
+current_round = 1
+
+print("Game on!")
+print(f"Player 1: {player_1}")
+print(f"Player 2: {player_2}")
+print("----------------------")
+
+print(f"Round {current_round}")
+player_1_score = 10
+player_2_score = 13
+print(f"{player_2} wins with {player_2_score} to {player_1_score}")
+print("----------------------")
+
+print(f"Round {current_round +1}")
+player_1_score = 20
+player_2_score = 5
+print(f"{player_1} defeats {player_2} by {player_1_score - player_2_score} points")
