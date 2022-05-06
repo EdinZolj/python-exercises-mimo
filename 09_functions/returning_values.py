cce8d819ff6d31da48b6cc4f5a33c5d159a39cec
EdinZolj <edinzolj@gmx.de>
2022-05-06 13:25:47 +0200

    New exercise

---
+++ 09_functions/returning_values.py
@@ -0,0 +1,26 @@
+#A function can return a value to the code that called it. 
+#To return something from a function we add the return keyword followed by the value to return.
+#A function can return any type of value.
+
+def age_label(age):
+    lable = "User age: " + age
+    return lable                    #if we dont include a return statement, the function returns the value None instead.
+
+result = age_label("29")            #we can store the return value in a variale too.
+print(result)
+
+
+def add_greeting(user):
+    greeting = "Greetings " + user
+    return greeting
+
+result = add_greeting("Simon")
+print(result)
+
+
+def sign_up(user):
+    status = user + " signed up"
+    return status
+
+result = sign_up("Desmond")
+print(result)
