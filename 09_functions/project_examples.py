236015918a5427dc91493baa911064fc2b984e38
EdinZolj <edinzolj@gmx.de>
2022-05-11 15:59:51 +0200

    New exercise

--- 09_functions/project_examples.py
+++ 09_functions/project_examples.py
@@ -1,1 +1,5 @@
-# Morse Encoder - 
+# Morse Encoder - Program converts a set of numbers to morse code.
+# We will replace each number of the passed code parameter with its morse correspondent and return the result.
+
+def convert_to_morse(code):
+    code = code.replace
