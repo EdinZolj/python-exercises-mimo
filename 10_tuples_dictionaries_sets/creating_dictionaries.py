a4c3b9b5848105f1c2b6b6eb0db159112e439799
EdinZolj <edinzolj@gmx.de>
2022-05-18 14:43:38 +0200

    New exercise

---
+++ 10_tuples_dictionaries_sets/creating_dictionaries.py
@@ -0,0 +1,5 @@
+# Dictonery - assosiate a meaning to each value in a collection of values. 
+# They are made up of pairs of keys "key:value"
+
+locations = {"headquarters": "New Yourk", "flagship": "Paris"}
+print(locations)
