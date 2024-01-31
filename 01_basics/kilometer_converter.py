6f75a5f9979f1d6068bfea108aa516ebd9b91d12
EdinZolj <edinzolj@gmx.de>
2024-01-31 21:07:20 +0100

    Update

--- 01_basics/kilometer_converter.py
+++ 01_basics/kilometer_converter.py
@@ -3,3 +3,12 @@
 miles = 500
 kilometers = miles * 1.609344
 print(kilometers)
+
+#1. Create a greeting for your program.
+print("Welcome to the Startup Name Generator.")
+#2. Ask the user for the city that they grew up in.
+first_name = input("What's the name of the city you grew up in?\n")
+#3. Ask the user for the name of a pet.
+color_name = input("What's your favorite color?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your startup name coud be " + first_name + " " + color_name)
